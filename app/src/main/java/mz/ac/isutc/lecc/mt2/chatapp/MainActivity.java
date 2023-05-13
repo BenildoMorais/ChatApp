1bd54f439b6b0c2a22dd935d593b470216c53a23
BenildoMorais <benildomorais360@gmail.com>
2023-05-13 20:08:57 +0200

    começando com o main

--- app/src/main/java/mz/ac/isutc/lecc/mt2/chatapp/MainActivity.java
+++ app/src/main/java/mz/ac/isutc/lecc/mt2/chatapp/MainActivity.java
@@ -1,18 +1,49 @@
 package mz.ac.isutc.lecc.mt2.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import mz.ac.isutc.lecc.mt2.chatapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()) {
+                    String uid = dataSnapshot.getKey();
+                    if (!uid.equals(FirebaseAuth.getInstance().getUid())) {
+                        UserModel userModel = dataSnapshot.child(uid).getValue(UserModel.class);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
