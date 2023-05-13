117630ea13791facedc231bb769173e013a9e2fb
BenildoMorais <benildomorais360@gmail.com>
2023-05-13 07:44:30 +0200

    commit inicial

---
+++ app/src/main/java/mz/ac/isutc/lecc/mt2/chatapp/MainActivity.java
@@ -0,0 +1,14 @@
+package mz.ac.isutc.lecc.mt2.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
