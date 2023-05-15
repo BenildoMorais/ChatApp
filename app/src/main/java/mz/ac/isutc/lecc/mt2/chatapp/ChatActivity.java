c0daf007aea7638fba06c510fc1eeba9137128f0
nielsero <niels.cpp@gmail.com>
2023-05-15 20:04:03 +0200

    fix chat messages

--- app/src/main/java/mz/ac/isutc/lecc/mt2/chatapp/ChatActivity.java
+++ app/src/main/java/mz/ac/isutc/lecc/mt2/chatapp/ChatActivity.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 import mz.ac.isutc.lecc.mt2.chatapp.databinding.ActivityChatBinding;
